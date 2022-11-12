96e822fe74f15c904d147ce36c242dc371057fb0
juungchull <89003952+juungchull@users.noreply.github.com>
2022-11-12 16:36:04 +0900

    calendar

--- lib/src/pages/calendar_page.dart
+++ lib/src/pages/calendar_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:paged_vertical_calendar/paged_vertical_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return PagedVerticalCalendar(
+      onDayPressed: (value) {},
     );
   }
 }
